73af2338341497dfcc0d147b0664da607195c997
mbeaulieu22 <22.mbeaulieu@gmail.com>
2022-03-11 08:54:36 -0500

    adding char frequency, success rate up to 92%

--- Trie.py
+++ Trie.py
@@ -1,30 +1,38 @@
 class Trie:
-    class TrieNode:
-        def __init__(self):
+    class TrieNode():
+        def __init__(self, frequency=0):
             self.children = {}
+            self.frequency = frequency
 
-    def __init__(self):
+    def __init__(self, words):
         self.head_node = self.TrieNode()
         self.must_contain = set()
+        self.char_frequency = {}
+        self.insert_words(words)
+
+    def insert_words(self, words):
+        for word in words:
+            for c in word:
+                self.char_frequency[c] = 1 + self.char_frequency.get(c, 0)
+            self.insert_word(word)
 
     def insert_word(self, word):
         current_node = self.head_node
 
         for c in word:
             if c not in current_node.children:
-                current_node.children[c] = self.TrieNode()
+                current_node.children[c] = self.TrieNode(self.char_frequency[c])
             current_node = current_node.children[c]
 
     def get_possible_words(self):
-        possible_words = self.get_possible_words_helper(self.head_node, "", self.must_contain.copy())
-        return possible_words
+        return self.get_possible_words_helper(self.head_node, "", 0, self.must_contain.copy())
 
-    def get_possible_words_helper(self, node, prefix_string, must_contain):
+    def get_possible_words_helper(self, node, prefix_string, frequency_score, must_contain):
         result = []
 
         if len(prefix_string) == 5:
             if not must_contain:
-                return [prefix_string]
+                return [(prefix_string, frequency_score)]
             else:
                 return []
 
@@ -33,19 +41,29 @@ class Trie:
             if child[0] in must_contain_copy:
                 must_contain_copy.remove(child[0])
 
-            result += self.get_possible_words_helper(child[1], prefix_string + child[0], must_contain_copy)
+            if child[0] not in prefix_string:
+                frequency_score = frequency_score + child[1].frequency
+
+            result += self.get_possible_words_helper(child[1],
+                                                     prefix_string + child[0],
+                                                     frequency_score,
+                                                     must_contain_copy)
+            if child[0] not in prefix_string:
+                frequency_score = frequency_score - child[1].frequency
+
         return result
 
     def prune(self, guess, guess_results):
         self.must_contain = set()
         for index in range(5):
             if guess_results[index] == "🟩":
+                self.must_contain.add(guess[index])
                 self.prune_green(index, guess[index], self.head_node)
             elif guess_results[index] == "🟨":
                 self.must_contain.add(guess[index])
                 self.prune_yellow(index, guess[index], self.head_node)
             else:
-                if self.get_char_dict(guess)[guess[index]] < 2:
+                if guess[index] not in self.must_contain:
                     self.prune_red(index, guess[index], self.head_node)
                 # TODO: Prune double letter
 
--- main.py
+++ main.py
@@ -6,31 +6,34 @@ from Trie import Trie
 
 
 class WordleSolver:
-    def __init__(self, answer=""):
+    def __init__(self, answer="", debug_mode=False):
         lower_words = [word.lower() for word in words.words()]
         self.possible_words = list(filter(lambda w: len(w) == 5, lower_words))
-        self.trie = self.create_trie()
+        self.trie = Trie(self.possible_words)
+        self.debug_mode = debug_mode
         answer = answer if answer else self.get_random_word()
         self.state = GameState(answer)
-
-    def create_trie(self):
-        trie = Trie()
-        for word in self.possible_words:
-            trie.insert_word(word)
-        return trie
 
     def get_random_word(self):
         return random.choice(self.possible_words)
 
-    def get_random_guess(self):
-        return random.choice(self.trie.get_possible_words())
+    def get_guess_word(self):
+        possibilities = self.trie.get_possible_words()
+        possibilities.sort(key=lambda p: p[1], reverse=True)
+        return random.choice(possibilities[0:10])[0]
 
     def update_trie(self, guess, guess_result):
         self.trie.prune(guess, guess_result)
 
     def make_guess(self):
-        guess = self.get_random_guess()
+        guess = self.get_guess_word()
+        if self.debug_mode:
+            print(guess)
         self.state.make_guess(guess)
+
+        if self.debug_mode:
+            print(self.state.guess_results[-1])
+
         self.update_trie(guess, self.state.guess_results[-1])
 
 
@@ -39,18 +42,24 @@ def play_game():
 
     while not solver.state.game_over():
         solver.make_guess()
+    print("Won: {}, Answer: {}, Guesses: {}".format(solver.state.won,
+                                                    solver.state.answer,
+                                                    len(solver.state.guess_results)))
     return solver.state
 
 
 if __name__ == '__main__':
-    win_count = 0
-    for test in range(1000):
+    win_count, guess_count = 0, 0
+    game_count = 1000
+    for test in range(game_count):
         state = play_game()
         if state.won:
+            guess_count += len(state.guess_results)
             win_count += 1
-        else:
-            print("{} Lost on: {}".format(test, state.answer))
 
-    print("Won {} / 1000 Games!").format(win_count)
+    print("Won {} / {} Games! Average guess of {} on wins"
+          .format(win_count,
+                  game_count,
+                  float(guess_count) / win_count))
 
 
